37b6430830767a443804add755bf1996ca7b651b
Aysegul Soytas Celik <aysegulsoytaszb@gmail.com>
2023-01-03 17:05:39 +0000

    Added second update

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,5 +1,7 @@
 public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello TEst");
+
+        System.out.println("About to share project on GitHub");
     }
 }
